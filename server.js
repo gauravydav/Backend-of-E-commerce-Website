a4a04d123d01072101a923b6a2076ea34624d398
gauravyadv <gauravyadavry_cse18@its.edu.in>
2023-11-04 20:11:56 +0530

    3rd commit

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
-import morgan from 'morgan'
+import dotenv from "dotenv";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js             '
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from './routes/categoryRoutes.js'
 
 //configure.env
 dotenv.config();
 connectDB();
 //test object
-const app=express();
+const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
-const PORT=process.env.PORT ||8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT,()=>{
-    console.log(`Server Running on ${process.env.DEV_MODE} on ${PORT}` )
-})
+app.listen(PORT, () => {
+  console.log(`Server Running on ${process.env.DEV_MODE} on ${PORT}`);
+});
